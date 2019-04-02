35793199653a74c722f788ca22c936392cf3c8dc
Felipe Corrionero <fcorrionero@gmail.com>
2019-04-02 20:04:42 +0200

    Main color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,30 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  static final Map<int, Color> colors = {
+    50:Color.fromRGBO(250,114,104, .1),
+
+    100:Color.fromRGBO(250,114,104, .2),
+
+    200:Color.fromRGBO(250,114,104, .3),
+
+    300:Color.fromRGBO(250,114,104, .4),
+
+    400:Color.fromRGBO(250,114,104, .5),
+
+    500:Color.fromRGBO(250,114,104, .6),
+
+    600:Color.fromRGBO(250,114,104, .7),
+
+    700:Color.fromRGBO(250,114,104, .8),
+
+    800:Color.fromRGBO(250,114,104, .9),
+
+    900:Color.fromRGBO(250,114,104, 1),
+  };
+  final MaterialColor colorCustom = MaterialColor(0xFFfa7268, colors); // 0xFFfa7268
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +42,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: colorCustom,
+        bottomAppBarColor: colorCustom
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Alpha'),
     );
   }
 }
@@ -92,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button:',
             ),
             Text(
               '$_counter',
